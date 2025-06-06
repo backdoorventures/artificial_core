bd7ae34df9af103d2fac01536f2cbff207c4bb2f
backdoorventures <backdoordigitalventures@gmail.com>
2025-06-06 19:43:23 -0400

    Update voiceover.py

--- generator/launchlayer_video/voiceover.py
+++ generator/launchlayer_video/voiceover.py
@@ -5,7 +5,7 @@ from google.oauth2 import service_account
 
 # Load credentials from env (as JSON string)
 credentials = service_account.Credentials.from_service_account_info(
-    eval(os.getenv("GOOGLE_SERVICE_ACCOUNT"))
+    json.loads(os.getenv("GOOGLE_SERVICE_ACCOUNT"))
 )
 
 tts_client = texttospeech.TextToSpeechClient(credentials=credentials)
